daa1ba449de820f6dbe3917f599858de9e5a7476
Jonas De Vuyst <jdv@foobar.be>
2014-05-16 22:05:13 +0700

    Added a third approach in the benchmark. This approach uses for, filter, and argument destructuring. It's slower than the membero approach at 50 nodes and edges.

--- test/comprehend/benchmark.clj
+++ test/comprehend/benchmark.clj
@@ -27,7 +27,7 @@
 
 (println "Constructing a graph")
 
-(def n 150)
+(def n 100)
 (def G (time (vec (make-graph n n))))
 
 (println "Constructing an equivalent indexed set")
@@ -40,19 +40,32 @@
                      S
                      (edge a b) (edge b c) (edge c d) (edge d e)
                      [a b c d e]))))
+;(println "Found " (count v1) " edges")
 
-(println "Finding paths using unification without indexes")
+(println "Finding paths using core.logic's membero and unification without indexes")
 
 (def v2 (time (vec (l/run* [a b c d e]
                            (l/membero (edge a b) G)
                            (l/membero (edge b c) G)
                            (l/membero (edge c d) G)
                            (l/membero (edge d e) G)))))
+;(println "Found " (count v2) " edges")
+
+(println "Finding paths using for, filter, and argument destructuring")
+
+(def v3 (time (let [E (vec (filter #(= 2 (count %)) G))]
+                (vec (->> (for [a E b E c E d E]
+                            [a b c d])
+                          (filter (fn [[[v1 v2a] [v2b v3a] [v3b v4a] [v4b v5]]]
+                                    (and (= v2a v2b)
+                                         (= v3a v3b)
+                                         (= v4a v4b)))))))))
+;(println "Found " (count v3) " edges")
 
 (print "Sanity checks... ")
 
 (assert (= (* 2 n) (count G) (count S)))
-(assert (= (count v1) (count v2)))
+(assert (= (count v1) (count v2) (count v3)))
 (assert (> (count v1) 0) "Try generating more edges")
 
 (println "OK")
