1b25fa4b99bc6a4d7bf4b96373373c681f7817d9
Jonas De Vuyst <jdv@foobar.be>
2014-08-07 17:31:36 +0700

    Updated core.logic dependency.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.logic "0.8.7"]])
+                 [org.clojure/core.logic "0.8.8"]]
+  :profiles {:dev {:dependencies [[org.clojure/math.combinatorics "0.0.7"]]}})
